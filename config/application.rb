97d8a6eff32880e9d0d25143a04367d8d709b3c0
jasondoc3 <jasondoc3@gmail.com>
2019-01-21 21:22:28 -0800

    Strip all unneeded code

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,30 @@
-require_relative 'boot'
-
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+require 'bundler/setup' # Set up gems listed in the Gemfile.
 require "rails"
 require "action_controller/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require
 
 module KafkaCommandSolo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    routes.append do
+      mount KafkaCommand::Engine, at: "/"
+    end
+
+    config.cache_classes = true
+    config.eager_load = true
+    config.log_level = :info
+    config.secret_key_base = ENV["SECRET_KEY_BASE"] || SecureRandom.hex(30)
+    config.assets.compile = false
+
+    if ENV["RAILS_LOG_TO_STDOUT"] != "disabled"
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
   end
 end
